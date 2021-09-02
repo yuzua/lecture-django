8238f7364aa243cb03afa0206528dfbd305b7816
ゆずき <k.saito70329@gmail.com>
2021-09-02 15:59:36 +0900

    [update] prevent template name duplication

--- diary/urls.py
+++ diary/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 app_name = 'diary'
 urlpatterns = [
-    path('',views.index, name ='index'),
+    path('',views.indexView.as_view(), name ='index'),
 ]
--- diary/views.py
+++ diary/views.py
@@ -1,6 +1,10 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
+from django.views import generic
 
 # Create your views here.
 def index(request):
     return render(request, 'diary/index.html')
+
+class IndexView(generic.TemplateView):
+    template_name="diary/index.html"
