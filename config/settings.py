d7359988ed40538ff54627233b074c2659aa56b1
yuzua <k.saito70329@gmail.com>
2021-09-08 12:26:37 +0900

    [update] Mail sending function

--- config/settings.py
+++ config/settings.py
@@ -122,3 +122,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR,'static'),
 )
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
--- diary/views.py
+++ diary/views.py
@@ -1,7 +1,11 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.views import generic
+from django.urls import reverse_lazy
 from .forms import InquiryForm
+import logging
+
+logger = logging.getLogger(__name__)
 
 # Create your views here.
 # def index(request):
@@ -13,3 +17,9 @@ class IndexView(generic.TemplateView):
 class InquiryView(generic.FormView):
     template_name="diary/inquiry.html"
     form_class = InquiryForm
+    success_url = reverse_lazy('diary:inquiry')
+
+    def form_valid(self, form):
+        form.send_email()
+        logger.info('Inquiry set by {}'.format(form.cleaned_data['name']))
+        return super().form_valid(form)
